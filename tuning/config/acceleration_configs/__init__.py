2bacf120ee2566250cad1f7ee3bcefb913faf671
1000960000 user <aaron.chew1@ibm.com>
2024-08-01 16:20:58 +0000

    add arguments to activate ilab plugin

--- tuning/config/acceleration_configs/__init__.py
+++ tuning/config/acceleration_configs/__init__.py
@@ -16,3 +16,4 @@
 from .acceleration_framework_config import AccelerationFrameworkConfig
 from .fused_ops_and_kernels import FusedOpsAndKernelsConfig
 from .quantized_lora_config import QuantizedLoraConfig
+from .instruct_lab_config import InstructLabConfig
